bed134074dac64b133919fbcf00c765102aa5d59
andi035 <119591927+andi035@users.noreply.github.com>
2023-03-22 17:24:58 +0100

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "6222408835:AAGxtk0YfVlGzB96Cwrpv22Qs2FRGwkqAYo"
+    OWNER_ID = "5147282548"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "https://t.me/Andreas035"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -21,20 +21,6 @@ class Config(object):
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
     URL = None
-
-    # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = None  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = False
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
-    BMERNU_SCUT_SRELFTI = 0
 
 class Production(Config):
     LOGGER = False
