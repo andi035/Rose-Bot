71f27f247a0f164867ac179741f2bb0d0cf76201
andi035 <119591927+andi035@users.noreply.github.com>
2023-03-22 17:53:36 +0100

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -1,3 +1,11 @@
+if not __name__.endswith("sample_config"):
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
+
+
+# Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
 
@@ -6,6 +14,13 @@ class Config(object):
     OWNER_ID = "5147282548"  # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "https://t.me/Andreas035"
 
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
 
 class Production(Config):
     LOGGER = False
@@ -13,3 +28,7 @@ class Production(Config):
 
 class Development(Config):
     LOGGER = True
+class Config(object):
+    LOGGER = True
+
+    
